4d2c0b7adebaa5d5949f7ec69ec8389c05e60826
nicole-hedley <nicole.k.hedley.15@dartmouth.edu>
2014-09-24 21:31:07 -0400

    added css and js files and frontend for homepage

---
+++ cs98jam/jam/static/jam/handlers.js
@@ -0,0 +1,15 @@
+var addContact = document.getElementById('addcontact');
+var addCompany = document.getElementById('addcompany');
+var addEvent = document.getElementById('addevent');
+
+addContact.addEventListener('click', function(){
+    console.log("This will create a contact!");
+});
+
+addCompany.addEventListener('click', function(){
+    console.log("This will create a company!");
+});
+
+addEvent.addEventListener('click', function(){
+    console.log("This will create an event!");
+});
