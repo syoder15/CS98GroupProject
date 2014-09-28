e7e866e045c230c9761a26766e7d0e717413d3be
Avalon McRae <avalonmcrae@gmail.com>
2014-09-28 16:36:55 -0400

    Created profile form, fixed link from home page

--- cs98jam/jam/urls.py
+++ cs98jam/jam/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, url
 
 from jam import views
 
-urlpatterns = patterns('', url(r'^$', views.index, name='index'))
+urlpatterns = patterns('', 
+    url(r'^$', views.index, name='index'),
+    url(r'profile', views.profile, name='profile'))
