41b83dc6d1d9b0b370ce7445d2d287ce72ef36a7
syoder15 <sam@yoder.org>
2014-09-28 15:39:41 -0400

    UI on homepage

--- cs98jam/jam/static/jam/handlers.js
+++ cs98jam/jam/static/jam/handlers.js
@@ -3,15 +3,19 @@ var addCompany = document.getElementById('addcompany');
 var addEvent = document.getElementById('addevent');
 
 addContact.addEventListener('click', function(){
-	var overlay = document.getElementById('overlay');
-	overlay.style.display = "block";
+	var contactOverlay = document.getElementById('contact_overlay');
+	contactOverlay.style.display = "block";
     console.log("This will create a contact!");
 });
 
 addCompany.addEventListener('click', function(){
+	var companyOverlay = document.getElementById('company_overlay')
+	companyOverlay.style.display = "block";
     console.log("This will create a company!");
 });
 
 addEvent.addEventListener('click', function(){
+	var eventOverlay = document.getElementById('event_overlay')
+	eventOverlay.style.display = "block";
     console.log("This will create an event!");
 });
