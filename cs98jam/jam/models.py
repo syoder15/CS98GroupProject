60d090460479d1292086fde4216346173c99508b
syoder15 <sam@yoder.org>
2014-09-29 17:49:08 -0400

    profile models

--- cs98jam/jam/models.py
+++ cs98jam/jam/models.py
@@ -15,3 +15,25 @@ class Contact(models.Model):
 
     name = models.CharField(max_length=50)
     phone_number = models.IntegerField(default=0)
+
+
+class Profile(models.Model):
+	def __unicode__(self):
+		return self.name
+
+	first_name = models.CharField(max_length=20)
+	last_name = models.CharField(max_length=50)
+	email = models.EmailField(max_length=60)
+	phone_number = models.IntegerField(default=0)
+	address = models.CharField(max_length=50)
+	city = models.CharField(max_length=40)
+	state = models.CharField(max_length=13)
+	zip_code = models.CharField(max_length=6)
+
+	#An error may occur here, unsure as to what rep of radiobutton is
+	gender = models.IntegerField(blank=True)
+
+	school = models.CharField(max_length=50)
+
+	grad_month = models.CharField(max_length=10)
+	grad_year = models.IntegerField(default = 0000, max_length=5)
