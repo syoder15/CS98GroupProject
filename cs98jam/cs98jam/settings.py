b012ccd89263ba08fb9e2843bc5a85f737e3d036
Janice Yip <janice.yip.15@dartmouth.edu>
2014-09-28 23:59:04 -0400

    merge

--- cs98jam/cs98jam/settings.py
+++ cs98jam/cs98jam/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jquery',
-    'south',
+    # 'jquery',
+    # 'south',
     'jam'
 )
 
