d6be264ce9219823dfe73b412af6b39d2921c954
Graeson McMahon <Graeson.C.McMahon.15@dartmouth.edu>
2014-10-05 13:15:04 -0400

    Password change

--- cs98jam/cs98jam/settings.py
+++ cs98jam/cs98jam/settings.py
@@ -43,6 +43,14 @@ INSTALLED_APPS = (
     'twitter_bootstrap'
 )
 
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'dartmouthjam@gmail.com'
+EMAIL_HOST_PASSWORD = 'biggreenjam'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'dartmouthjam@gmail.com'
+
 ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window; you may, of course, use a different value.
 
 MIDDLEWARE_CLASSES = (
--- cs98jam/cs98jam/urls.py
+++ cs98jam/cs98jam/urls.py
@@ -19,4 +19,19 @@ urlpatterns = patterns('',
     (r'^login/$', 'django.contrib.auth.views.login'),
     (r'^logout/$', logout_page),
 	(r'^register/$', register),
+	
+	url(r'^password_change/reset/$', 
+        'django.contrib.auth.views.password_reset', 
+        {'post_reset_redirect' : '/password_change/done/'},
+        name="password_reset"),
+		
+		 (r'^password_change/done/$',
+        'django.contrib.auth.views.password_reset_done'),
+		
+		(r'^password_change/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', 
+        'django.contrib.auth.views.password_reset_confirm', 
+        {'post_reset_redirect' : '/password_change/complete/'}),
+		
+		(r'^password_change/complete/$', 
+        'django.contrib.auth.views.password_reset_complete'),
 )
