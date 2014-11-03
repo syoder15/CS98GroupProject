62a7c794214ff4fc8e3307f5eb519b98ad47dd82
Avalon McRae <avalonmcrae@gmail.com>
2014-11-03 17:45:30 -0500

    Comapny page

---
+++ cs98jam/jam/static/js/events.js
@@ -0,0 +1,8 @@
+var links = $('.calLink');
+
+/* Change button color when clicked/on that page */
+links.click( function() {
+	links.removeClass('activeButton');
+	$(this).addClass('activeButton');
+});
+	console.log(links);
