fe8276b87a28b0c36c4c318a733e5a87d6b12b50
Avalon McRae <avalonmcrae@gmail.com>
2014-11-05 16:50:14 -0500

    Company files

---
+++ cs98jam/jam/migrations/0021_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('jam', '0019_channel_events'),
+        ('jam', '0020_contact_notes'),
+    ]
+
+    operations = [
+    ]
