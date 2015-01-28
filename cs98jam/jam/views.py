89d9b1c7a7cd8bd24f051834fc71906506f8f345
Graeson McMahon <Graeson.C.McMahon.15@dartmouth.edu>
2015-01-27 21:35:03 -0500

    More event improvements

--- cs98jam/jam/views.py
+++ cs98jam/jam/views.py
@@ -89,7 +89,7 @@ def index(request):
 		c_name = ""
 
 		context = {'username': request.user.username, 'upload_form': upload_form, 'site': site, 
-			'channels': channels, 'show': show_feed ,'events': events, 'notificationList': notificationList, 'app_list': app_notifications,
+			'channels': channels, 'show': show_feed ,'events': future_events, 'notificationList': notificationList, 'app_list': app_notifications,
 			'article_urls': article_urls, 'article_images': article_images}
 
 	#post request can mean 2 things.
@@ -897,7 +897,7 @@ def add_event(
 				
 			#### JAM CODE ####	
 			recurrence_form.save(event)
-			return http.HttpResponseRedirect(event.get_absolute_url())
+			return http.HttpResponseRedirect(event.occurrence_set.first().get_absolute_url())
 	else:
 		if 'dtstart' in request.GET:
 			try:
@@ -919,7 +919,7 @@ def add_event(
 		{'dtstart': dtstart, 'event_form': event_form, 'recurrence_form': recurrence_form, 'username': request.user.username}
 	)
 
-   ####FROM SWINGWIME ADD COMENTS
+   ####FROM SWINGTIME ADD COMMENTS
 
 def event_listing(
 	request,
