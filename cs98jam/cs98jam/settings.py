05662fd94e48d75993afe6a4417d68e32e79df43
Kim <Kimberly.a.strauch.15@dartmouth.edu>
2015-02-04 12:51:41 -0500

    settings fix

--- cs98jam/cs98jam/settings.py
+++ cs98jam/cs98jam/settings.py
@@ -58,7 +58,7 @@ INSTALLED_APPS = (
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'dartmouthjam@gmail.com'
-EMAIL_HOST_PASSWORD = 'biggreenjam'
+EMAIL_HOST_PASSWORD = 'spacejam98'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'dartmouthjam@gmail.com'
