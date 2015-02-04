12a895f470ce6a0aba1f2d26270264bcb69288d6
Kim <Kimberly.a.strauch.15@dartmouth.edu>
2015-02-04 12:46:20 -0500

    fixed pword

--- cs98jam/cs98jam/local_settings.py
+++ cs98jam/cs98jam/local_settings.py
@@ -5,6 +5,6 @@ ACCOUNT_ACTIVATION_DAYS = 7
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "dartmouthjam@gmail.com"
-EMAIL_HOST_PASSWORD = "biggreenjam"
+EMAIL_HOST_PASSWORD = "spacejam98"
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL= "dartmouthjam@gmail.com"
